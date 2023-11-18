0f058b1bea259b37e85ad2763554b6b878069d01
oscar-m-sandria <oscar.sandria@gmail.com>
2023-11-18 04:12:32 -0600

    HelloWorld.c v1.0 using using <<printf()>> function.

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,24 @@
+/*
+
+program....: HelloWorld.c
+version....: 1.0
+date.......: 18-Nov-2023
+IDE........: Dev C++ 5.11, Apr. 27, 2015
+Github user: oscar-m-sandria
+purpose....: To show some basic C programming language
+Tip........: Compile and execute with F11 in Dev C++ IDE.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	printf("\n");	/* new line */
+	printf("Hello World (coded in Dev C++ 5.11 IDE)!");
+	printf("\n");	/* new line */
+	
 	return 0;
 }
